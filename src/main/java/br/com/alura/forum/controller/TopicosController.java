041b7223222e14fb8d34482a8f14757dde8c9f38
IgorSMendes2001 <igor07082001@gmail.com>
2022-08-26 09:24:23 -0300

    Alterações paginação

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,8 +22,8 @@ public class TopicosController {
 	private TopicoRepository topicoRepository;
 	
 	@RequestMapping("/topicos")
-	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,@RequestParam int pagina ,@RequestParam int qntd) {
-		Pageable paginacao= PageRequest.of(pagina,qntd);
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,@RequestParam int pagina ,@RequestParam int qntd,@RequestParam String ordenacao) {
+		Pageable paginacao= PageRequest.of(pagina,qntd, Sort.Direction.ASC,ordenacao);
 		if (nomeCurso == null) {
 			Page<Topico> topicos = topicoRepository.findAll(paginacao);
 			return TopicoDto.converter(topicos);
