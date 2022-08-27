7a45c4337f10881b63b0e34ba1d02f8bdb0a3d1d
IgorSMendes2001 <igor07082001@gmail.com>
2022-08-27 15:14:19 -0300

    Adicionando Swagger

--- src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -52,7 +52,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     //Configurações de Recursos estáticos(js,css,imagens,etc..)
     @Override
     public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**","/configuration/**", "/swagger-resources/**");
     }
-
-
 }
